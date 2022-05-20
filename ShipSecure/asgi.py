923c3c468fbdf78fd7c6cadac83b59216c49f919
sarimahmad <sarimahmadqafi@gmail.com>
2022-05-20 21:49:07 +0500

    Live7

--- ShipSecure/asgi.py
+++ ShipSecure/asgi.py
@@ -13,13 +13,14 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.conf.urls import url
 from django.urls import path
-from django.core.asgi import get_asgi_application
+import django
+# from django.core.asgi import get_asgi_application
 from shipment.consumers import *
-
+from channels.routing import get_default_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ShipSecure.settings')
+django.setup()
 
-
-application = get_asgi_application()
+application = get_default_application()
 
 websocket_urlpatterns = [
     path('ws/Shipment/<str:room_id>/', MyConsumer.as_asgi())
