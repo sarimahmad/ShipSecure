90abaeee333ae941f1d8d98f2dfc4981c2dd0d9e
sarimahmad <sarimahmadqafi@gmail.com>
2022-06-04 21:18:48 +0500

    RedisIssue

--- ShipSecure/settings/production.py
+++ ShipSecure/settings/production.py
@@ -6,3 +6,21 @@ ALLOWED_HOSTS = ['ship-secure.herokuapp.com']
 DEBUG = True
 
 DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=True)}
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+        },
+    },
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+   }
+}
